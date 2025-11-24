26f64ac2112681aa78c426768896a8f0cbfbe7cc
Vatsal2007 <vatsalchawla8@gmail.com>
2025-11-25 00:00:05 +0530

    106.c

---
+++ 106.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of array:");
+    scanf("%d", &n);
+
+    int arr[n];
+     printf("Enter the elements:");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        int next = -1;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+                next = arr[j];
+                break;
+            }
+        }
+
+        printf("%d", next);
+        if (i != n - 1) printf(", ");
+    }
+
+    return 0;
+}
