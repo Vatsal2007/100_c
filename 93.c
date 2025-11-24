766c796e209fd7878fe3f9b262f47f5e2b8e72dd
Vatsal2007 <vatsalchawla8@gmail.com>
2025-11-24 23:35:15 +0530

    93.c

---
+++ 93.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+    char str1[1000], str2[1000];
+    int freq[256] = {0}; 
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+    for (int i = 0; str1[i] != '\0'; i++) {
+        if (str1[i] != '\n')
+            freq[(unsigned char)str1[i]]++;
+    }
+    for (int i = 0; str2[i] != '\0'; i++) {
+        if (str2[i] != '\n')
+            freq[(unsigned char)str2[i]]--;
+    }
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] != 0) {
+            printf("Not anagrams\n");
+            return 0;
+        }
+    }
+    printf("Anagrams\n");
+    return 0;
+}
